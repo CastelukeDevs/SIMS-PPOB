008360c2963a0a67ff37a1839aefd4b6dfeee12f
Mashipro <captain.neon99@google.com>
2024-01-06 20:50:38 +0700

    Chore: Shorthand Absolute Import

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,33 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
-  plugins: ['react-native-reanimated/plugin'],
+  plugins: [
+    'react-native-reanimated/plugin',
+    [
+      'module-resolver',
+      {
+        extensions: [
+          '.ios.js',
+          '.android.js',
+          '.ios.jsx',
+          '.android.jsx',
+          '.js',
+          '.jsx',
+          '.json',
+          '.ts',
+          '.tsx',
+        ],
+        root: ['.'],
+        alias: {
+          '@Common': './src/Components/Common',
+          '@Components': './src/Components',
+          '@Utilities': './src/Utilities',
+          '@Types': './src/Types',
+          '@Screens': './src/Screens',
+          '@Routes': './src/Routes',
+          '@Resources': './src/Resources',
+          '@Redux': './src/Redux',
+        },
+      },
+    ],
+  ],
 };
