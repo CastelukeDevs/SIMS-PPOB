49ca1363d37431b20b4b7046d17acbecccddcd7b
Mashipro <captain.neon99@google.com>
2024-01-07 19:13:42 +0700

    New: Format currency tools

---
+++ src/Utilities/Tools/FormatCurrency.ts
@@ -0,0 +1,21 @@
+/**
+ * Format Number into Currency format
+ * @param amount number
+ * @returns symbol "Rp"
+ * @returns whole "10.000"
+ * @returns decimal ",00"
+ * @returns format "Rp 10.000,00"
+ */
+export default (amount: number) => {
+  const symbol = 'Rp';
+  const [wholeNum, decimalNum] = amount.toString().split('.');
+
+  const fixDecimal = decimalNum || '00';
+  const decimal = ',' + fixDecimal;
+
+  const whole = wholeNum.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.');
+
+  const format = `${symbol} ${whole}${decimal}`;
+
+  return {symbol, whole, decimal, format};
+};
